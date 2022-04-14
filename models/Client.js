13a420dd3f3fbbc8105d6a268bbbb268d26d56f6
mickovi <m.oviedo@uni.pe>
2022-04-14 10:59:03 -0500

    Definido el modelo Client

---
+++ models/Client.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+// TODO: RUC/DNI
+const ClientSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        trim: true
+    },
+    enterprise: {
+        type:  String,
+        require: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: 'Seller'
+    }
+});
+
+module.exports = mongoose.model('Client', ClientSchema);
