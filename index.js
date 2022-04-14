d61aa0bf63588a9a52b633f339cf582bfe6b2cf2
mickovi <m.oviedo@uni.pe>
2022-04-14 11:00:20 -0500

    Agregado el contexto

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./gql/schema');
 const resolvers = require('./gql/resolvers');
 const connectDB = require('./config/db');
+// Para autenticar el cliente de un vendedor:
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variable.env'} );
 
 // Conectar a la base de datos
 connectDB();
@@ -9,7 +12,22 @@ connectDB();
 // Crear una instancia de ApolloServer
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({ req }) => {
+        // console.log(req.headers['authorization']);
+        const token = req.headers['authorization'] || '';
+        if (token) {
+            try {
+                const seller = jwt.verify(token, process.env.AUTH_KEY);
+                // console.log(seller);
+                return {
+                    seller
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
 });
 
 // Arrancar el servidor
