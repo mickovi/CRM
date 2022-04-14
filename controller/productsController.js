a877803e2a2a8cb171883b9bede9f09f1e75dcaa
mickovi <m.oviedo@uni.pe>
2022-04-14 10:57:35 -0500

    Comentarios arreglados

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,9 +1,31 @@
 const Product = require('../models/Product');
 
+// QUERY
+
+const getProduct = async id => {
+    try {
+        const product = await Product.findById(id);
+        if (!product) throw new Error('Producto no encontrado');
+        return product;
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+const getProducts = async () => {
+    try {
+        const products = await Product.find({});
+        return products;
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+// MUTATION
 
 const newProduct = async input => {
 
-    // 3. Guardar el registro del vendedor en la base de datos.
+    // Guardar el registro del vendedor en la base de datos.
     try {
         const product = new Product(input);
         const request = await product.save();
@@ -13,6 +35,34 @@ const newProduct = async input => {
     }
 };
 
+const updateProduct = async (id, input) => {
+    let product = await Product.findById(id);
+
+    // 1. Validar la existencia del producto
+    if (!product) throw new Error('Producto no encontrado');
+    
+    // 2. Actualizar y guardar en la base de datos
+    product = await Product.findOneAndUpdate({_id: id}, input, {new: true});
+
+    return product;
+};
+
+const deleteProduct = async id => {
+    let product = await Product.findById(id);
+
+    // 1. Validar la existencia del producto
+    if (!product) throw new Error('Producto no encontrado');
+
+    // 2. Eliminar
+    await Product.findOneAndDelete({_id: id});
+
+    return 'Producto eliminado!'
+}
+
 module.exports = {
+    getProduct,
+    getProducts,
     newProduct,
+    updateProduct,
+    deleteProduct,
 };
--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const ProductSchema = mongoose.Schema ({
+const ProductSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
